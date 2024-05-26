22d152419439fcdf2e518d0e2232945ed6d95f04
Shweta Rawat <shwetarawat524@gmail.com>
2024-05-26 10:49:49 +0530

    Edited the landing page.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,26 @@
-import "./Landing.css"
-import Navbar from '../../components/common/Navbar/Navbar';
-import MainDashboard from '../../components/common/MainDashboard/MainDashboard';
-import Footer from '../../components/common/Footer/Footer';
-const LandingPage = () => {
-    return (<>
+
+import React from 'react';
+// import Navbar from './Navbar'; Don't do this
+import Navbar from '../../components/landingNavbar/Navbar';
+import Header from '../../components/landingHeader/Header';
+import Body from '../../components/landingBody/Body';
+import Steps from '../../components/landingSteps/Steps';
+import Footer from '../../components/landingFooter/Footer';
+
+
+
+function LandingPage() {
+    return (
+      <div className="LandingPage">
+        {/* LandingPage */}
         <Navbar />
-        <MainDashboard />
-        <Footer />
+        <Header/>
+        <Body/>
+        <Steps/>
+        <Footer/>
 
-    </>
-    )
-}
-
-export default LandingPage;
+      </div>
+    );
+  }
+  
+  export default LandingPage;
